ea622f0af307e4420edbb5166d092dfbb20cf7e9
samir-27 <sumrasamir27@gmail.com>
2024-10-09 15:37:58 +0530

    login user

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
+const jwtsecret = process.env.JWT_SECRET
 
 exports.createUser = [
   body("email", "Please enter a valid email").isEmail(),
@@ -29,6 +30,59 @@ exports.createUser = [
         success: true,
         data: response,
         message: "User created successfully",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  },
+];
+
+exports.loginUser = [
+  body("email", "Please enter a valid email").isEmail(),
+  body("password", "Password must be at least 6 characters long").isLength({
+    min: 6,
+  }),
+  async (req, res) => {
+    try {
+      const validationError = validationResult(req);
+      if (!validationError.isEmpty()) {
+        return res.status(400).json({ errors: validationError.array() });
+      }
+
+      const { email, password } = req.body;
+      let userData = await User.findOne({ email });
+      if (!userData) {
+        return res.status(400).json({
+          success: false,
+          message: "credentials invalid",
+        });
+      }
+
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        userData.password
+      );
+      if (!pwdCompare) {
+        return res.status(400).json({
+          success: false,
+          message: "credentials invalid",
+        });
+      }
+      const data = {
+        user: {
+          id: userData.id,
+        },
+      };
+
+      const authToken = jwt.sign(data,jwtsecret);
+      res.status(200).json({
+        success: true,
+        authToken: authToken,
+        message: "user login successfully",
       });
     } catch (err) {
       console.log(err);
